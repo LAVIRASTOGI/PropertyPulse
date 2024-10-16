fc7dce0aff2a3964abdf4e1144ac0b7ff03e28b9
LaviRastogi <1993lavirastogi@gmail.com>
2024-10-16 10:36:06 +0530

    featired

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,12 @@
-import Hero from '../components/Hero';
-import InfoBoxes from '@/components/InfoBoxes';
-import HomeProperties from '@/components/HomeProperties';
-import FeaturedProperties from '@/components/FeaturedProperties';
+import Hero from "../components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
-      <FeaturedProperties />
       <HomeProperties />
     </>
   );
